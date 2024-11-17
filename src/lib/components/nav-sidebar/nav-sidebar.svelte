4bee87ec5b31e504f30058f00ce1edf545e5d43a
Daniel Riccio <ricciodaniel98@gmail.com>
2024-11-17 17:39:48 -0500

    implement navigation

--- src/lib/components/nav-sidebar/nav-sidebar.svelte
+++ src/lib/components/nav-sidebar/nav-sidebar.svelte
@@ -1,13 +1,44 @@
-<script>
-  import { Home, SquareActivity } from 'lucide-svelte';
-  import { Button } from '../ui/button';
+<script lang="ts">
+  import { Cable, SquareActivity } from "lucide-svelte";
+  import { Button } from "../ui/button";
+  import * as Tooltip from "../ui/tooltip";
+  import { page } from "$app/stores";
+
+  let path = $state($page.url.pathname);
+
+  $effect(() => {
+    path = $page.url.pathname;
+  });
 </script>
 
 <aside
   class="bg-background w-16 flex flex-col items-center space-y-2 py-4 border-r"
 >
-  <Button class="w-10 h-10" variant="ghost" size="icon"><Home /></Button>
-  <Button class="w-10 h-10" variant="ghost" size="icon"
-    ><SquareActivity /></Button
-  >
+  <Tooltip.Root>
+    <Tooltip.Trigger class="w-10 h-10">
+      <Button
+        class="w-10 h-10"
+        href="/"
+        variant={path === "/" ? "default" : "ghost"}
+        size="icon"><Cable /></Button
+      >
+    </Tooltip.Trigger>
+    <Tooltip.Content>
+      <p>Connections</p>
+    </Tooltip.Content>
+  </Tooltip.Root>
+
+  <Tooltip.Root>
+    <Tooltip.Trigger class="w-10 h-10">
+      <Button
+        class="w-10 h-10"
+        href="/monitor"
+        variant={path === "/monitor" ? "default" : "ghost"}
+        size="icon"><SquareActivity /></Button
+      >
+    </Tooltip.Trigger>
+    <Tooltip.Content>
+      <p>Monitor</p>
+    </Tooltip.Content>
+  </Tooltip.Root>
 </aside>
